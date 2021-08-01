ae07544ed299413cc710cce1a54a69e301ee7c8a
norbyscook <andyqe@gmail.com>
2021-08-01 16:42:35 -0700

    renamed StoryIntroStates to NonPlayStates

---
+++ Assets/Scripts/StateTextsScripts/NonPlayStates.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// states for the menus like story intro, ending, and main menu 
+[CreateAssetMenu(menuName = "State Texts/Non Play States")]
+public class NonPlayStates : StatesTextTemplate
+{
+}
