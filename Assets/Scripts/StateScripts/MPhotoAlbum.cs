a52f7b717baa062c0f538404860ebb81712517ba
norbyscook <andyqe@gmail.com>
2021-08-05 18:29:26 -0700

    added more game objects for states added a new location class script

---
+++ Assets/Scripts/StateScripts/MPhotoAlbum.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CanvasProject
+{
+    public class MPhotoAlbum : StatesTemplate
+    {
+        public MPhotoAlbum()
+        {
+            lable = "School Bag";
+        }
+    }
+}
